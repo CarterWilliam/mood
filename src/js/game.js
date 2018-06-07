5c492041507c7e7ba5a8b097eac8db78e06fdba5
Will Carter <williamcarter1988@gmail.com>
2018-06-07 23:23:50 +0100

    empty town map

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,4 @@
+import { Depth } from './constants'
 import Soldier from './sprites/soldier'
 import Enemy from './sprites/enemy'
 import Imp from './sprites/imp'
@@ -24,30 +25,40 @@ var config = {
   }
 };
 
+const SCALE = 2
+
 var game = new Phaser.Game(config);
-var map;
-var cursors;
-var player, imp;
+var cursors
+var player, imp
 
 function preload() {
-  this.load.image('tiles', 'assets/images/magecity.png')
+  this.load.image('tiles', 'assets/maps/emptytown.png')
 
   this.load.image('bullet', 'assets/images/bullet.png')
   this.load.spritesheet('fireball', 'assets/images/fireball.png', { frameWidth: 15, frameHeight: 15 })
 
-  this.load.tilemapTiledJSON('map', 'assets/maps/corridor-town.json');
+  this.load.tilemapTiledJSON('map', 'assets/maps/emptytown.json');
   this.load.spritesheet('soldier', 'assets/images/soldier.png', { frameWidth: 60, frameHeight: 60 });
   this.load.spritesheet('imp', 'assets/images/imp.png', { frameWidth: 60, frameHeight: 60, endFrame: 68 });
 }
 
 function create() {
-  map = this.make.tilemap({ key: 'map' });
-  var tileset = map.addTilesetImage('magecity', 'tiles')
-  var floor = map.createStaticLayer('floor', tileset, 0, 0);
-  var onTheFloor = map.createStaticLayer('overfloor', tileset, 0, 0);
-  var obstructions = map.createStaticLayer('obstructions', tileset, 0, 0);
+  let map = this.make.tilemap({ key: 'map' });
+  let tileset = map.addTilesetImage('emptytown', 'tiles')
 
-  obstructions.setCollisionByProperty({ collide: true });
+  let floor = map.createStaticLayer('floor', tileset, 0, 0);
+  floor.setCollisionByProperty({ collide: true });
+  floor.setScale(SCALE, SCALE)
+
+  let onTheFloor = map.createStaticLayer('onthefloor', tileset, 0, 0);
+  onTheFloor.setCollisionByProperty({ collide: true });
+  onTheFloor.setScale(SCALE, SCALE)
+
+  let foreground = map.createStaticLayer('foreground', tileset, 0, 0);
+  foreground.setScale(SCALE, SCALE)
+  foreground.setDepth(Depth.FOREGROUND)
+
+  this.physics.world.setBounds(0, 0, 32*16*SCALE, 64*16*SCALE)
 
   createAnimations(this)
 
@@ -56,16 +67,18 @@ function create() {
   player = new Soldier({
     scene: this,
     key: 'soldier',
-    x: 100, y: 300,
+    x: 32*10, y: 32*10,
     projectiles: playerProjectiles
   })
+
+  this.cameras.main.startFollow(player, true)
 
   let enemyProjectiles = new Projectiles(this)
 
   imp = new Enemy({
     scene: this,
     key: 'imp',
-    x: 200, y: 200,
+    x: 32*20, y: 32*20,
     health: 60,
     speed: 50,
     sight: 100,
@@ -77,11 +90,13 @@ function create() {
     },
     shootDuration: 4000})
 
-  this.physics.add.collider(player, obstructions);
-  this.physics.add.collider(imp, obstructions);
+  this.physics.add.collider(player, floor);
+  this.physics.add.collider(player, onTheFloor);
+  this.physics.add.collider(imp, floor);
+  this.physics.add.collider(imp, onTheFloor);
   this.physics.add.collider(player, imp);
 
-  this.physics.add.collider(playerProjectiles, obstructions, function(projectile, tile) {
+  this.physics.add.collider(playerProjectiles, floor, function(projectile, tile) {
     projectile.destroy()
   });
 
@@ -90,7 +105,7 @@ function create() {
     projectile.destroy()
   })
 
-  this.physics.add.collider(enemyProjectiles, obstructions, function(projectile, tile) {
+  this.physics.add.collider(enemyProjectiles, floor, function(projectile, tile) {
     projectile.destroy()
   })
 
@@ -98,7 +113,6 @@ function create() {
     projectile.destroy()
   })
 
-  var foreground = map.createStaticLayer('foreground', tileset, 0, 0);
 
   cursors = this.input.keyboard.createCursorKeys();
 }
